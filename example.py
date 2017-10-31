4183741ae4bf9b683e6898e2cff8ef88c554393a
Albie Baker-Smith <bakersmitha@gmail.com>
2017-10-31 16:00:57 +0000

    Displays the sentence

--- example.py
+++ example.py
@@ -3,6 +3,8 @@ import sys
 
 sentence = "Go to the end of the corridor" if len(sys.argv) == 1 else sys.argv[1]
 
+print("SENTENCE: " + setence)
+
 tokens = nltk.word_tokenize(sentence)
 print("TOKENS: " + str(tokens))
 
