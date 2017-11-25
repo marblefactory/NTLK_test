b3f307472f2d9899d4585478d2dd7f8c47f102df
Albie Baker-Smith <bakersmitha@gmail.com>
2017-11-25 12:48:35 +0000

    Chagned func name

--- example.py
+++ example.py
@@ -1,7 +1,7 @@
 import nltk
 import sys
 
-sentence = "Go to the end of the corridor" if len(sys.argv) == 1 else sys.argv[1]
+sentence = "Run down to room 6" if len(sys.argv) == 1 else sys.argv[1]
 
 print("SENTENCE: " + sentence)
 
@@ -10,3 +10,27 @@ print("TOKENS: " + str(tokens))
 
 tagged = nltk.pos_tag(tokens)
 print("TAGS: " + str(tagged))
+
+
+def get_obj_name(text: str) -> str:
+    """
+    :param text: the text the user spoke.
+    :return: the name of the object the user is referring to.
+    """
+    tokens = nltk.word_tokenize(sentence)
+    tagged = nltk.pos_tag(tokens)
+    nouns = [(i, word) for i, (word, tag) in enumerate(tagged) if tag == 'NN']
+
+    if nouns is None:
+        raise Exception('No place name found')
+
+    i, noun = nouns[0]
+
+    # Find the name of the room.
+    if noun == 'room' and i+1 < len(tokens):
+        room_name = tokens[i+1]
+        return 'room ' + room_name
+
+    return noun
+
+print('PLACE: ' + get_obj_name(sentence))
